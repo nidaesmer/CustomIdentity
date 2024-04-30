4e869c6ea2cf7f5a5f0524a4bd089940d0a45289
nidaesmer <esmerertugral@gmail.com>
2024-04-30 16:51:43 +0300

    Add project files.

---
+++ CustomIdentity/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using CustomIdentity.Models;
+using CustomIdentity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomIdentity.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly SignInManager<AppUser> signInManager;
+		public  AccountController(SignInManager<AppUser> signInManager)
+		{
+			this.signInManager = signInManager;
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public async Task<IActionResult> LoginAsync(LoginVM model)
+        {
+			if(ModelState.IsValid)
+			{
+				//login
+				var result = await signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe, false);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+				ModelState.AddModelError("", "Invalid login attemp");
+				return View(model);
+			}
+            return View(model);
+        }
+
+
+        public IActionResult Register()
+		{
+			return View();
+		}
+
+		public IActionResult Logout()
+		{
+			return View();
+		}
+	}
+}
---
+++ CustomIdentity/ViewModels/RegisterVM.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.ViewModels
+{
+	public class RegisterVM
+	{
+		[Required]
+		public string? Name { get; set; }
+
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string? Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string? Password { get; set; }
+
+		[Compare("Password", ErrorMessage = "Passwords don't match.")]
+		public string? ConfirmPassword { get; set; }
+
+
+		//çok satırlı metin olacagını belırtmek ıcın
+		[DataType(DataType.MultilineText)]
+		public string? Adress {  get; set; }
+
+	}
+}
